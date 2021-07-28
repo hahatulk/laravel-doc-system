204056242326f0342956ff44c415a9bc36dc2121
Vadim Golyatkin <goljatkin2128506@gmail.com>
2021-07-28 14:56:34 +0300

    1

--- app/Http/Controllers/DocumentRequestController.php
+++ app/Http/Controllers/DocumentRequestController.php
@@ -3,27 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrdersLkRequest;
-use App\Http\Requests\OrdersSummaryRequest;
 use App\Models\DocumentRequest;
 use App\Models\User;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Support\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use Request;
 
 class DocumentRequestController extends Controller {
-    public function lk(OrdersLkRequest $request) {
-        if (Auth::user()->role === User::ROLE_STUDENT) {
+    public function lk(OrdersLkRequest $request): JsonResponse {
+        if (Auth::user()->role === User::ROLE_ADMIN) {
             $query = DocumentRequest::summary();
 
-            $this->success($query);
+            return $this->success($query);
         }
 
         if (Auth::user()->role === User::ROLE_STUDENT) {
-            $query = DocumentRequest::getList();
+            $query = DocumentRequest::getList()->get();
 
-            $this->success($query);
+            return $this->success($query);
         }
+
+        return $this->error();
+
     }
 
 }
