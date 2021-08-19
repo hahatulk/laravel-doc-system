baded4bda6aac8ba11cb65085c301026724cf0aa
Vadim Golyatkin <goljatkin2128506@gmail.com>
2021-08-19 18:21:18 +0300

    fihi

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.localhost/', '*.google.com:*', '*.edo.pedcollchelny.ru:*'],
+    'allowed_origins' => ['*.google.com:*', '*.edo.pedcollchelny.ru:*'],
 
     'allowed_origins_patterns' => [],
 
